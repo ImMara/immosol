cba91a38066edc1b9768dff9003c86563cbaa2b2
Mara <alaneraerts@live.be>
2021-06-20 22:39:07 +0200

    gallery + no upload

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # EXPRESS_MVC_TEMPLATE
 
 npm start ( development )
-npm start:prod ( production )
+npm start:prod ( production )i
 npm scss ( scss process )
 
 add .env to gitignore
--- controllers/locations.controller.js
+++ controllers/locations.controller.js
@@ -1,3 +1,4 @@
+const {createType} = require("../database/queries/type.queries");
 const {findAllType} = require("../database/queries/type.queries");
 const {updateLocation} = require("../database/queries/locations.queries");
 const {deleteLocation} = require("../database/queries/locations.queries");
@@ -26,6 +27,18 @@ exports.formLocations = async (req,res,next) =>{
 exports.createLocations = async(req,res,next) =>{
     try{
         const body = req.body;
+        const type = await  findAllType()
+
+        // for (let typeKey in type) {
+        //     console.log(1,type[typeKey].title)
+        //     console.log(2,body.type)
+        //     if( body.type !== type[typeKey].title){
+        //         await createType({ title : body.type})
+        //     }
+        // }
+
+        console.log(body.gallery)
+
         const loca = {
             title:body.title,
             contact:{
@@ -44,8 +57,10 @@ exports.createLocations = async(req,res,next) =>{
                 parking:body.parking? body.parking :null,
                 kitchen:body.kitchen? body.kitchen :null,
                 pool:body.pool? body.pool :null,
-                type:body.type
+                type:body.type,
             },
+            image: body.image,
+            gallery: body.gallery,
             featured:body.featured? body.featured :null,
             description:body.description,
             cost:body.cost
@@ -106,7 +121,7 @@ exports.updateLocation = async(req,res,next) => {
         await updateLocation(id,loca)
 
         const locations = await findAllLocations();
-        res.render('locations/index',{ locations , currentUser:req.user})
+        res.render('locations/index',{ locations , currentUser:req.user })
 
     }catch (e) {
         next(e)
