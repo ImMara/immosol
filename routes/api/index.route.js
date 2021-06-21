e3c621f021f87a44644e369e7db4f09774124170
Mara <alaneraerts@live.be>
2021-06-21 19:56:13 +0200

    changes

--- routes/api/index.route.js
+++ routes/api/index.route.js
@@ -3,6 +3,6 @@ const {example} = require("../../controllers/api.controller");
 const router = require('express').Router();
 
 router.get('/' , example )
-router.get('/delete/gallery/:id/:pos',deleteOne)
+router.put('/delete/gallery/:id/:pos',deleteOne)
 
 module.exports = router;
