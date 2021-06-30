69978b0275d5dc304279b33ded4c5638f338613c
Mara <alaneraerts@live.be>
2021-06-30 20:31:27 +0200

    headers cors

--- app.js
+++ app.js
@@ -32,6 +32,28 @@ app.set('view engine', 'pug');
 require('./config/session.config');
 require('./config/passport.config');
 
+
+// *********  APP HEADER  *********
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // *********  BEHAVIOR  *********
 
 app.use(morgan('short'));
