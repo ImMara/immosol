aaae2581545c8f484d9e74322627551e455a2f7e
Mara <alaneraerts@live.be>
2021-07-05 05:07:36 +0200

    eco

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps : [{
+    name: 'immosol',
+    script: './app.js',
+    instances: 'max',
+    autorestart: true,
+    watch: true,
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+};
