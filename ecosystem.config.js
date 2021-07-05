62487bc076f3bea6c9f04c78ad512b6a72e1ebf6
Mara <alaneraerts@live.be>
2021-07-05 05:55:23 +0200

    no env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,5 @@ module.exports = {
     instances: 'max',
     autorestart: true,
     watch: true,
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
   }],
 };
